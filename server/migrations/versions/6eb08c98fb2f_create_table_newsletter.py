e80b89adbca8ce7d6351484b083194dabe3ff48f
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-04-05 14:15:23 -0600

    code along

---
+++ server/migrations/versions/6eb08c98fb2f_create_table_newsletter.py
@@ -0,0 +1,24 @@
+"""create table newsletter
+
+Revision ID: 6eb08c98fb2f
+Revises: 2da2c0ccb068
+Create Date: 2023-04-05 13:39:49.674373
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6eb08c98fb2f'
+down_revision = '2da2c0ccb068'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
